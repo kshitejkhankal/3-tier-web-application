81f1a637571069ee0cfdfe087e347c93e33b7c5e
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-09-13 18:05:45 +0530

    change-name

--- subnet-association-rt.tf
+++ subnet-association-rt.tf
@@ -2,9 +2,4 @@ resource "aws_route_table_association" "a" {
     subnet_id = aws_subnet.public-sub.id
     route_table_id = aws_route_table.terraform-pub-sub-rt.id
   
-
-route {
-    cidr_block = "30.0.1.0/24"
-    gateway_id = aws_internet_gateway.terraform-vpc-igw.id
-}
 }
